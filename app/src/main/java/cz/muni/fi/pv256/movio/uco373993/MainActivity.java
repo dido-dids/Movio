72884342d0d8802b623fbcb283242fa5ec5f254b
David Boron <xboron@nymfe63.fi.muni.cz>
2015-10-12 09:41:28 +0200

    cviko4

--- app/src/main/java/cz/muni/fi/pv256/movio/uco373993/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco373993/MainActivity.java
@@ -1,22 +1,43 @@
 package cz.muni.fi.pv256.movio.uco373993;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Random random = new Random();
+
+        List<Bar> data = new ArrayList<Bar>(20);
+        for (int i = 0; i < 20; i++) {
+            data.add(new Bar(random.nextInt()));
+        }
+        setListAdapter(new FooAdapter(this, data));
+
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +47,59 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private static class FooAdapter extends BaseAdapter {
+
+        private List<Bar> data;
+        private Context context;
+
+        public FooAdapter(Context context, List<Bar> data) {
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return data.get(position).i;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+            }
+
+            TextView tv = (TextView) convertView;
+            tv.setText(String.valueOf(data.get(position).i));
+
+            return convertView;
+        }
+    }
+
+    private static class Bar{
+
+        public int i;
+
+        public Bar(int i){
+            this.i = i;
+        }
+
+
+
+
+
     }
 
     @Override
