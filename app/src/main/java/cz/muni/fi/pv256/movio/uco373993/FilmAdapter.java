fcc2b476c904d2f4501414ed666eea961ccdb6de
DiDo <malyborky@gmail.com>
2015-10-16 02:25:31 +0200

    uloha4(2)

--- app/src/main/java/cz/muni/fi/pv256/movio/uco373993/FilmAdapter.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco373993/FilmAdapter.java
@@ -1,7 +1,51 @@
 package cz.muni.fi.pv256.movio.uco373993;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 /**
  * Created by David Boron on 16.10.2015.
  */
-public class FilmAdapter {
+public class FilmAdapter extends ArrayAdapter<Film> {
+    private Context mContext;
+    private int mLayoutResourceid;
+
+    public FilmAdapter(Context context, int resource, List<Film> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mLayoutResourceid = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = ((Activity) mContext).getLayoutInflater().inflate(mLayoutResourceid, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.mImageView = (ImageView) convertView.findViewById(R.id.row);
+            convertView.setTag(viewHolder);
+            Log.i("adapter","inflate radku "+ position);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            Log.i("adapter","recyklace radku "+ position);
+        }
+
+
+
+        Picasso.with(mContext).load(getItem(position).getCoverPath()).into(viewHolder.mImageView);
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView mImageView;
+    }
 }
--- app/src/main/java/cz/muni/fi/pv256/movio/uco373993/MovieDetailactivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco373993/MovieDetailactivity.java
@@ -3,11 +3,11 @@ package cz.muni.fi.pv256.movio.uco373993;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MovieDetailactivity extends AppCompatActivity {
+public class MovieDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_detailactivity);
+        setContentView(R.layout.activity_movie_detail);
     }
 }
